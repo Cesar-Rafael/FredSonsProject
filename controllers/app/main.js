3d5f8e0a83068b3357c9d384757a1bd888fe28f3
Cesar <crafael@pucp.edu.pe>
2022-01-16 02:25:13 +0000

    Some improvements

--- controllers/app/main.js
+++ controllers/app/main.js
@@ -1,31 +1,28 @@
 const router = require('express').Router()
 const token = require('../../db/models/token')
-const cheerio = require('cheerio')
-const fetch = require('superagent')
+const TwitterKeys = require('../../config/twitter')
+const TwitterClient = require('twitter-api-client').TwitterClient
+const SerpApi = require('google-search-results-nodejs')
 const { getData, postData } = require('../utils/fetch')
 const { urlencode, clientId, clientSecret } = require('../../config/linkedin')
-const { apiKey } = require('../../config/google')
-const { PythonShell } = require('python-shell')
-const { getDirectoryProject } = require('../utils/getDirectoryProject')
-const dirProject = getDirectoryProject(__dirname, 1);
-const pythonScript = `${dirProject}/utils/getUserProfile.py`
-const TwitterClient = require('twitter-api-client').TwitterClient
-const twitterKeys = require('../../config/twitter')
-const SerpApi = require('google-search-results-nodejs')
-const search = new SerpApi.GoogleSearch(apiKey)
+const GoogleKeys = require('../../config/google')
 
-// Twitter
+// Google Search
+const googleClient = new SerpApi.GoogleSearch(GoogleKeys.apiKey)
 
+// Twitter: Doesn't work because the free plan for developers is limited, so we can't search profiles with completely name
 const twitterClient = new TwitterClient({
-    apiKey: twitterKeys.apiKey,
-    apiSecret: twitterKeys.apiKeySecret,
-    accessToken: twitterKeys.accessToken,
-    accessTokenSecret: twitterKeys.accessTokenSecret,
+    apiKey: TwitterKeys.apiKey,
+    apiSecret: TwitterKeys.apiKeySecret,
+    accessToken: TwitterKeys.accessToken,
+    accessTokenSecret: TwitterKeys.accessTokenSecret,
 })
 
+// Main route API
 router.route('/')
     .get(async (req, res) => {
         try {
+            // This functionality is for linkedin APIs, to get the access tokens and others
             let code = req.query.code
             if (code) {
                 // Update the token
@@ -35,138 +32,52 @@ router.route('/')
                 // Save the access token
                 await token.findOneAndUpdate({ id: 2 }, { code: response.access_token }, { new: true })
             }
-            res.render('app/index')
         } catch (e) {
             console.log(e)
-            res.render('app/index')
         }
-    })
-
-router.route('/search')
-    .get(async (req, res) => {
-        let result = {}
-        try {
-            //let response = await getData('https://api.linkedin.com/v2/people?ids=List((id:291498206))')
-            //res.render('app/index')
-
-            let title = await fetch.get('https://www.linkedin.com/in/carlos-melgarejo-roman-291498206/').then(response => {
-                var $ = cheerio.load(response.text)
-                var title = $('title').text()
-                return title
-            }).catch(e => {
-                console.log(e)
-                return ''
-            })
-
-            result.content = { title }
-            res.json(result).status(200)
-        } catch (e) {
-            console.log(e)
-            res.render('app/index')
-        }
-
         res.render('app/index')
     })
 
-function convertToName(publicId) {
-    let array = publicId.split('-');
-    let name = '';
-    let flag = false;
-    array.forEach(word => {
-        flag = false;
-        for (let i = 0; i < word.length; i++) {
-            if (word[i] >= '0' && word[i] <= '9') {
-                flag = true;
-                break;
-            }
-        }
-        if (flag == false) {
-            name += ' ' + word;
-        }
-    });
-    return name;
-}
-
-router.route('/search-data')
+// API that searchs all person information with the linkedin URL
+router.route('/search-person-information')
     .post(async (req, res) => {
         let response = { result: 'NOK', content: {} }
 
         try {
-            const { publicUrl } = req.body
-            let publicId = publicUrl.split('/')[4]
-            let linkedinData = await postData('http://10.150.0.3:3000/get-linkedin-data', { publicId })
-            let name = linkedinData.firstName + linkedinData.lastName
-            response.content.linkedin = linkedinData
-            await search.json({
-                q: name, // reemplaza el nombre de busqueda
-                location: "Lima Region" // Usar la location de linkedin
+            const { publicUrl } = req.body // Get the linkedin publicUrl as input 
+            let publicId = publicUrl.split('/')[4] // Get the publicId which is the linkedin publicUrl last part 
+            let linkedinData = await postData('http://10.150.0.3:3000/get-linkedin-data', { publicId }) // Call the flask API that collects all the linkedin information 
+            let name = linkedinData.firstName + linkedinData.lastName // Get the name which will be the input for the Google Search
+            response.content.linkedin = linkedinData // Save the linkedin information in response
+            // Call the Google API for collects all the google information
+            await googleClient.json({
+                q: name, // The query search is the name person
+                location: "Lima Region" // The location is get it from the linkedin data
             }, (result) => {
-                //console.log(result)
-                //response.content.google = JSON.parse(result)
-                response.content.google = result
+                response.content.google = result // Save the google information in response
                 response.result = 'OK'
-                res.json(response).status(200)
             })
-
         } catch (e) {
             console.log(e)
         }
+
+        res.json(response).status(200)
     })
 
+// API that gets twitter information, but It's also limited for the basic plan, even so, we send a form expecting we can access to all APIs 
 router.route('/twitter/:query')
     .get(async (req, res) => {
         const query = req.params.query
-
-        twitterClient.tweets.search({
-            q: query
-        }).then((response) => {
-            res.send(response)
-        }).catch((err) => {
-            console.error(err)
-            res.status(500).send('An error occurred, please try again later.')
-        })
-    })
-
-router.route('/google')
-    .get(async (req, res) => {
-        await search.json({
-            q: "Franccesco Jaimes Agreda",
-            location: "Lima Region"
-        }, (result) => {
-            res.json({ content: result }).status(200)
-        })
-    })
-
-router.route('/test')
-    .get(async (req, res) => {
         let response = { result: 'NOK' }
-        try {
-            let linkedinData = await postData('http://10.150.0.3:3000/get-linkedin-data', { publicId: 'cesar-david-rafael-artica-a6a504199' })
-            response.content = linkedinData
-            response.result = 'OK'
-            res.json(response).status(200)
-        } catch (e) {
-            console.log(e)
-        }
-    })
 
-/*
-    res.render("/", {
-        // Lite fields
-        id: res.id,
-        firstName: res.firstname,
-        localizedFirstName: res.localizedFirstName,
-        lastName: res.lastName,
-        localizedLastName: res.localizedLastName,
-        profilePicture: res.profilePicture,
-        //Basic fields
-        maidenName : res.maidenName,
-        localizedMaidenName : res.localizedMaidenName,
-        headline : res.headline,
-        localizedHeadline : res.localizedHeadline,
-        vanityName : res.vanityName
-jwt // ????
-        });
-    */
+        try {
+            response.content = await twitterClient.tweets.search({ q: query })
+            response.result = 'OK'
+        } catch (e) {
+            console.error(e)
+        }
+
+        res.json(response).status(200)
+    })
 
 module.exports = router;
